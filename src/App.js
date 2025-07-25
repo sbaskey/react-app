9ba90ee9ba84fcaa866793421b67b0b3aa02dd02
sbaskey <sbaskey1998@gmail.com>
2025-07-25 09:45:34 +0530

    header footer added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import photo11 from "./photo11.jpg";
+import OnlineServices from "./OnlineServices.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          cicd presentation
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="header">
+        <img src={OnlineServices} alt="Logo" className="header-logo" />
+        {/* <span className="header-title">Fixed Header</span> */}
       </header>
+
+      <main className="main-content">
+        <div className="content-wrapper">
+          <img src={photo11} alt="Slide" className="plain-slide-image fall-animation image-fade-in" />
+
+          <div className="icon-text-container">
+            <div className="icon-text-box">
+              <img src="/images/icon1.png" alt="Icon 1" className="icon-image" />
+              <span className="icon-text">First Item</span>
+            </div>
+            <div className="icon-text-box">
+              <img src="/images/icon2.png" alt="Icon 2" className="icon-image" />
+              <span className="icon-text">Second Item</span>
+            </div>
+            <div className="icon-text-box">
+              <img src="/images/icon3.png" alt="Icon 3" className="icon-image" />
+              <span className="icon-text">Third Item</span>
+            </div>
+            <div className="icon-text-box">
+              <img src="/images/icon4.png" alt="Icon 4" className="icon-image" />
+              <span className="icon-text">Fourth Item</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="footer">Designed by @National Informatics Center</footer>
     </div>
   );
 }
